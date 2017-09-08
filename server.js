fbe53ddbe591f673b2e6ee18ad0b4993ded1c741
clbokea <clbo@kea.dk>
2017-09-08 12:33:15 +0200

    readfile added

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    res.writeHead(200, {'Content-type' : 'text/json'});
-    res.write('{"title" : "Hello JSON web API", "text" : "Text fra min API"}');
-    res.end();
-});
+    fs.readFile('big.txt', 'utf-8', function (err, data){
+        
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(200, {'Content-type' : 'text/json'});
+        res.write(data);
+        res.end();
 
+        console.log('Sendt til browser!');
+   });
+    
+});
+console.log('efter server metode');
 server.listen(process.env.PORT || 3003);
